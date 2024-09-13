1bfdbb807ac6daccd2b533bc3b948f01c12e8535
hye-min-jo <137854531+hye-min-jo@users.noreply.github.com>
2024-09-13 13:44:40 +0900

    함수내에 함수

---
+++ src/함수내에 함수 선언.kt
@@ -0,0 +1,15 @@
+fun sum(a:Int, b:Int):Int {
+    var sum =0
+    fun calSum(){
+        for(i in a..b){
+            sum += i
+        }
+    }
+    calSum()
+    return sum
+}
+
+fun main(args: Array<String>) {
+    val result=sum(1,10)
+    println(result)
+}
