e3c465b4f6576bfb7e55c1e6cce6b8c15005eaa1
hye-min-jo <137854531+hye-min-jo@users.noreply.github.com>
2024-09-19 16:13:39 +0900

    오버로딩과 중위 표현식

---
+++ src/오버로딩.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    some("hello")
+    some(10)
+    some(10,"hello")
+}
+
+fun some(a:String){
+    println("some(a:String) call...")
+}
+
+fun some(a:Int){
+    println("some(a:Int) call...")
+}
+
+fun some(a:Int, b:String){
+    println("some(a:Int,b:String) call...")
+}
